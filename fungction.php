adf88c1709299c848806e298fa16e7b169124f0c
dippssy74 <doniabil114@gmail.comm>
2026-07-12 17:17:40 +0700

    tambah session

--- fungction.php
+++ fungction.php
@@ -84,4 +84,37 @@
         return mysqli_affected_rows($conn);
     }
 
+    function registrasi($data) {
+        global $conn;
+
+        $username = strtolower(stripslashes($data["username"]));
+        $password = mysqli_real_escape_string($conn, $data["password"]);
+        $konfirmasi = mysqli_real_escape_string($conn, $data["konfirmasi"]);
+
+        // cek konfirmasi password
+        if($password !== $konfirmasi) {
+            echo "<script>
+                    alert('Konfirmasi password tidak sesuai!');
+                  </script>";
+            return false;
+        }
+
+        // cek username sudah ada atau belum
+        $result = mysqli_query($conn, "SELECT username FROM users WHERE username = '$username'");
+        if(mysqli_fetch_assoc($result)) {
+            echo "<script>
+                    alert('Username sudah terdaftar!');
+                  </script>";
+            return false;
+        }
+
+        // enkripsi password
+        $password = password_hash($password, PASSWORD_DEFAULT);
+
+        // tambahkan user baru ke database
+        mysqli_query($conn, "INSERT INTO users (username, password) VALUES ('$username', '$password')");
+
+        return mysqli_affected_rows($conn);
+    }
+
 ?>
